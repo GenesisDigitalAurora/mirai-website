0325d1449f1a46f55fe17713d9b922533d30eef6
GenesisDigitalAurora <genesis.digital2920@gmail.com>
2025-08-24 17:44:49 -0600

    Bold the language option selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,11 +1,11 @@
 export default function LanguageSelector() {
   return (
     <div className="flex items-center">
-      <span className="px-2 py-1 text-sm font-medium text-secondary">
+      <span className="text-weight-900 font-bold px-2 py-1 text-sm font-medium text-secondary">
         ES
       </span>
-      <span className="mx-1 text-white md:text-black">|</span>
-      <span className="px-2 py-1 text-sm font-medium text-white md:text-black">
+      <span className="text-weight-900 font-bold mx-1 text-white md:text-black">|</span>
+      <span className="text-weight-900 font-bold px-2 py-1 text-sm font-medium text-white md:text-black">
         EN
       </span>
     </div>
